005e32304ecd72f931b7891a406891970e78f385
Alejandra B Arguello <alejandrasrg53@gmail.com>
2025-07-10 19:57:18 -0300

    transformar fuction original en formato arrow fuction

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function saludar(){
+const capturarEtiqueta = () =>{
+    console.log("ingresando a función capturarEtiqueta()");
+    const parrafo = document.getElementById("parrafo");
+    return parrafo;
+}
+
+const saludar = () => {
     console.log ("saludando!");
-    
     const parrafo = document.getElementById("parrafo");
     parrafo.textContent = "cambiado!";
 }
