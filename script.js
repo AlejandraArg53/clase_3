d2b373442620b25112bfbc515ff218b196778cc8
Alejandra B. Argüello <aleecalivaubaguera@gmail.com>
2025-07-10 19:26:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function saludar(){
-    console.log ("saludando!");
-    
+function capturarEtiqueta(){
+    console.log ("ingresando a la función capturarEtiqueta()");
     const parrafo = document.getElementById("parrafo");
-    parrafo.textContent = "cambiado!";
+    return parrafo;
+}
+
+function saludar(){
+    const etiquetaP = capturarEtiqueta();
+    etiquetaP.textContent = "Hola Equipo Pilar Tecno"
+
 }
